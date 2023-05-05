76d367ef42e468de73082d639012c418ee825cba
Vitalii Hura <vitaliihura@gmail.com>
2023-05-05 13:03:54 +0300

    10 demo posts are created instead of 3

--- functions.php
+++ functions.php
@@ -203,7 +203,7 @@ function childtwentytwentyone_add_services_test_data()
     );
 
 
-    for ($i = 1; $i <= 3; $i++) {
+    for ($i = 1; $i <= 10; $i++) {
         $title = 'Service ' . $i;
         $content = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy.';
         $excerpt = substr($content, 0, 30);
